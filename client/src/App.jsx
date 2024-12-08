ef33c5e26ed17c8e6e21fe7ace758b3632919c79
VishalVyas106 <vyasvishal106.work@gmail.com>
2024-12-08 23:25:41 +0530

    Normalize line endings and update Cart import add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,65 +2,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+
+// Components
 import Navbar from './components/Navbar';
-import Home from './Pages/Home';
-import Menu from './pages/Menu';
-import Cart from './pages/Cart';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Orders from './pages/Orders';
-import AdminDashboard from './pages/AdminDashboard';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
+import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
+
+// Pages
+import Home from './Pages/Home';
+import Menu from './Pages/Menu';
+import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Orders from './Pages/Orders';
+import AdminDashboard from './Pages/AdminDashboard';
 import RajvenBooking from './Pages/reservation';
 import FoodDetailPage from './Pages/FoodDetailPage';
-import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <CartProvider>
-          <div className="App">
-            <Navbar />
-            <div className="AllPage">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                
-        <Route path="/food/:id" element={<FoodDetailPage />} />
-                <Route path="/Reservations" element={<RajvenBooking/>} />
-                <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
-                <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
-              </Routes>
-            </div>
-            <Toaster position="top-right" />
+// Context Providers
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
 
-    <Footer/>
-
+const App = () => (
+  <Router>
+    <AuthProvider>
+      <CartProvider>
+        <div className="App">
+          <Navbar />
+          <div className="AllPage">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/food/:id" element={<FoodDetailPage />} />
+              <Route path="/Reservations" element={<RajvenBooking />} />
+              <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
+              <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+            </Routes>
           </div>
-        </CartProvider>
-      </AuthProvider>
-    </Router>
-  );
-};
+          <Toaster position="top-right" />
+          <Footer />
+        </div>
+      </CartProvider>
+    </AuthProvider>
+  </Router>
+);
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
